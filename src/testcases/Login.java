769e98f2e29f004360d54586dc94e99e433dbc1a
unknown <Administrator@.(none)>
2016-04-12 16:28:55 +0800

    test

--- src/testcases/Login.java
+++ src/testcases/Login.java
@@ -29,6 +29,7 @@ public class Login extends DataProvide{
    /**
     * 登录，并检测登录（用户名）
     */
+	  //s
       comfun.login(dr.readnodevalue(params, "login", "username"), dr.readnodevalue(params, "login", "password"));
       comfun.checkequal(dr.readnodevalue(params, "login", "value1"),comfun.gettext("xpath", dr.readnodevalue(params, "login", "checkpoint1")));
    }
