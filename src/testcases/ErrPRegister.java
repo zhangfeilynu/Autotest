f893377477a7913020b87f6c91ae60de0d476cbd
unknown <Administrator@.(none)>
2016-04-22 18:43:28 +0800

    ErrpRegister.java

--- src/testcases/ErrPRegister.java
+++ src/testcases/ErrPRegister.java
@@ -1,9 +1,9 @@
 package testcases;
 
 import org.openqa.selenium.TakesScreenshot;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.AfterClass;
 import org.w3c.dom.Document;
 
 import commonfunction.CommonFunctions;
@@ -11,14 +11,14 @@ import commonfunction.DataProvide;
 import commonfunction.DataReader;
 
 
-//重复注册
+//注册时图形验证码错误
 
-public class ErrRegister extends DataProvide {
+public class ErrPRegister extends DataProvide {
 	
 	public CommonFunctions comfun;
 	public DataReader dr;
 	
-	@BeforeClass
+	@BeforeTest
 	public void beforeClass() throws Exception {
 		String url="http://test2.sui.me/";
 		comfun=new CommonFunctions(url);
@@ -29,31 +29,32 @@ public class ErrRegister extends DataProvide {
 	}
 	
     @Test(dataProvider = "Test_xml_dataprovider")
-    public void testerrreg(Document params) throws Exception {
+    public void testerrpreg(Document params) throws Exception {
     	    	  	
     	//输入用户名
-    	comfun.inputvalue("name",dr.readnodevalue(params,"HomePage","username"),"DupRegister");
+    	comfun.inputvalue("name",dr.readnodevalue(params,"HomePage","username"),"Register");
     	//输入已注册过的手机号
     	comfun.inputvalue("name",dr.readnodevalue(params,"HomePage","cellphone"),"13988880000");
     	//输入密码
     	comfun.inputvalue("name",dr.readnodevalue(params,"HomePage","password"),"123456");
-    	//输入图形验证码
+    	//输入错误的图形验证码
     	comfun.inputvalue("name",dr.readnodevalue(params,"HomePage","imgVerifyCode"),"qwer");
     	//点击获取按钮
     	comfun.clickitem("xpath",dr.readnodevalue(params,"HomePage","obtain"));
-    	//提示已注册
+    	//提示验证码错误
+    	Thread.sleep(1000);
     	comfun.checkequal("图形验证码错误",comfun.gettext("css",dr.readnodevalue(params,"HomePage","tips")));
   
     }
 
    
 
-    @AfterClass
+    @AfterTest
     public void afterClass() throws Exception {
     	/**
   	   * 截图、退出浏览器
   	   */
-    	CommonFunctions.snapshot((TakesScreenshot)CommonFunctions.driver,"ErrRegister.png");
+    	CommonFunctions.snapshot((TakesScreenshot)CommonFunctions.driver,"ErrPRegister.png");
     	comfun.teardown();
     }
 
